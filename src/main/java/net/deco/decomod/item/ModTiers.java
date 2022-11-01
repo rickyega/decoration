6e350a0a577f61c84eaedffb27fe0648c3019ebe
rickyega <rickyega@yahoo.ne.jp>
2022-11-01 13:30:06 +0900

    add hellstone tools

--- src/main/java/net/deco/decomod/item/ModTiers.java
+++ src/main/java/net/deco/decomod/item/ModTiers.java
@@ -5,7 +5,7 @@ import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraftforge.common.ForgeTier;
 
 public class ModTiers {
-    public static final ForgeTier HELLSTONE = new ForgeTier(5, 5000, 1.5f,
-            2f, 22, BlockTags.NEEDS_DIAMOND_TOOL,
+    public static final ForgeTier HELLSTONE = new ForgeTier(5, 2500, 12f,
+            5f, 25, BlockTags.NEEDS_DIAMOND_TOOL,
             () -> Ingredient.of(ModItems.HELLSTONE_INGOT.get()));
 }
